87e5fa3465003d3e2bee1f0014f8ec3a6a991cda
Saulo Rangel <saulorangel10@hotmail.com>
2026-08-17 07:46:07 -0300

    feat: implement App and Header components with styles

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Header from './components/Header.jsx'
+import styles from './App.module.css'
+
+export default function App() {
+  return (
+    <div className={styles.App}>
+      <Header />
+    </div>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import styles from './Header.module.css'
+
+export default function Header() {
+    return (
+        <div className={styles.Header}>
+            <h1>Controle de Gastos</h1>
+        </div>
+    )
+}
