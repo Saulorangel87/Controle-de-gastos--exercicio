e154b18cbd141ce4ff4efcc2ae753c1c87e09420
Saulo Rangel <saulorangel10@hotmail.com>
2026-08-17 21:24:35 -0300

    feat: refactor FormGasto component to include form handling and state management for expense entries

--- src/components/FormGasto.jsx
+++ src/components/FormGasto.jsx
@@ -2,12 +2,42 @@ import styles from "./FormGasto.module.css";
 import { useState } from "react";
 
 export default function FormGasto() {
-    const [gastos, setGastos] = useState([]);
+  const [gastos, setGastos] = useState([]);
+  const [valor, setValor] = useState(0);
+  const [descricao, setDescricao] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (descricao === "" || valor === 0) return;
+    setGastos([...gastos, { descricao, valor, id: Date.now() }]);
+    setDescricao("");
+    setValor(0);
+  };
+
   return (
     <div className={styles.FormGasto}>
-      <input type="text" placeholder="Descrição" />
-      <input type="number" placeholder="Valor" />
-      <button>+ Adicionar</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Descrição"
+          onChange={(e) => setDescricao(e.target.value)}
+          value={descricao}
+        />
+        <input
+          type="number"
+          placeholder="Valor"
+          onChange={(e) => setValor(e.target.value)}
+          value={valor}
+        />
+        <button type="submit">+ Adicionar</button>
+      </form>
+      <ul>
+        {gastos.map((gasto) => (
+          <li key={gasto.id}>
+            {gasto.descricao} - {gasto.valor}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
